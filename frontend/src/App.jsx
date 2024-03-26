4312f6b72151c4683e8567b4b1487aaeb0d69398
Maciej Klimek <87934529+maciej-klimek@users.noreply.github.com>
2024-03-26 12:24:06 +0100

    frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,49 @@
-import { useState, useEffect } from 'react'
-import RecipeList from './RecipeList'
-import './App.css'
+import { useState, useEffect } from "react";
+import RecipeList from "./RecipeList";
+import RecipeForm from "./RecipeForm";
+import "./App.css";
 
-function App() {
-  const [recipies, setRecipies] = useState([])
+const App = () => {
+  const [recipies, setRecipies] = useState([]);
+  const [recipeFormOpen, setRecipeFormOpen] = useState(false);
 
   useEffect(() => {
-    fetchRecipies()
-  }, [])
+    fetchRecipies();
+  }, []);
 
   const fetchRecipies = async () => {
     const response = await fetch("http://127.0.0.1:5000/recipies");
     const data = await response.json();
     setRecipies(data.recipies);
-    console.log(data.recipies)
-  }
+    console.log(data.recipies);
+  };
+
+  const closeRecipeForm = () => {
+    setRecipeFormOpen(false);
+  };
+
+  const openAddRecipeForm = () => {
+    if (!recipeFormOpen) setRecipeFormOpen(true);
+  };
+
+  const openUpdateRecipeForm = () => {};
 
   return (
-    <RecipeList recipes={recipies}/>
-  )
-}
+    <>
+      <RecipeList recipes={recipies} />
+      <button onClick={openAddRecipeForm}>Add a new recipe!</button>
+      {recipeFormOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close" onClick={closeRecipeForm}>
+              &times;
+            </button>
+            <RecipeForm />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/RecipeForm.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+const RecipeForm = ({ existingRecipe = {}, updateCallback }) => {
+  const [title, setTitle] = useState(existingRecipe.title || "");
+  const [recipeType, setRecipeType] = useState(existingRecipe.recipeType || "");
+  const [prepTime, setPrepTime] = useState(existingRecipe.prepTime || "");
+  const [ingredientList, setIngredientList] = useState(
+    existingRecipe.ingredientList || ""
+  );
+  const [instructions, setInstructions] = useState(
+    existingRecipe.instructions || ""
+  );
+
+  const updating = Object.entries(existingRecipe).length !== 0;
+
+  const submitRecipe = async (e) => {
+    e.preventDefault();
+    const recipeData = {
+      title,
+      recipeType,
+      prepTime,
+      ingredientList,
+      instructions,
+    };
+
+    const url =
+      "http://127.0.0.1:5000/" +
+      (updating ? `update_recipe/${existingRecipe.id}` : "add_recipe");
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(recipeData),
+    };
+
+    const response = await fetch(url, requestOptions);
+    if (response.status !== 201 && response.status !== 200) {
+      const responseData = await response.json();
+      alert(responseData.message);
+    } else {
+      updateCallback();
+      alert("giiiit");
+    }
+  };
+
+  return (
+    <form onSubmit={submitRecipe}>
+      <div>
+        <label htmlFor="title">Recipe Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="recipeType">Recipe Type:</label>
+        <input
+          type="text"
+          id="recipeType"
+          value={recipeType}
+          onChange={(e) => setRecipeType(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="prepTime">Preparation Time:</label>
+        <input
+          type="text"
+          id="prepTime"
+          value={prepTime}
+          onChange={(e) => setPrepTime(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="ingredientList">Ingredient List:</label>
+        <input
+          type="text"
+          id="ingredientList"
+          value={ingredientList}
+          onChange={(e) => setIngredientList(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="instructions">Instructions:</label>
+        <input
+          type="text"
+          id="instructions"
+          value={instructions}
+          onChange={(e) => setInstructions(e.target.value)}
+        />
+      </div>
+      <button type="submit">Add this recipe.</button>
+    </form>
+  );
+};
+
+export default RecipeForm;
